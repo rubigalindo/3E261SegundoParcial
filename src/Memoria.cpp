5b5b13c37f2522ebcdb56fdcc603bf55d1b92592
rubigalindo <a25110251@ceti.mx>
2026-05-17 00:52:31 +0000

    practica 6

---
+++ src/Memoria.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+ cout<< "int:" << sizeof  (long)<<endl;
+ cout<< "int:" << sizeof  (int)<<endl;
+cout<< "int:" << sizeof  (char)<<endl;
+ cout<< "int:" << sizeof  (bool)<<endl;
+cout<< "int:" << sizeof  (float)<<endl;
+ cout<< "int:" << sizeof  (double)<<endl;
+
+int a=5;
+
+cout << &a << endl;
+cout<< a << endl;
+cout<< *(&a)<< endl;
+cout <<sizeof(long*)<<endl;
+cout <<sizeof(int*)<<endl;
+cout <<sizeof(char*)<<endl;
+cout <<sizeof(bool*)<<endl;
+cout <<sizeof(float*)<<endl;
+cout <<sizeof(double*)<<endl;
+
+int* direccion;
+cout<<direccion<<endl;
+cout<<&direccion<<endl;
+cout<<*direccion<<endl;
+
+
+return 0;
+}
